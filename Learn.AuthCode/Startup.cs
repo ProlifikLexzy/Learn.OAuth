77d55d5373ab5e683565669d390b68d37ded7178
Prolifik Lexzy <pro_lifik@live.com>
2022-12-01 04:45:09 +0100

    initial code

---
+++ Learn.AuthCode/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn.AuthCode;
+public static class Startup
+{
+    public static void ConfigureAuth(this WebApplicationBuilder builder)
+    {
+        builder.Services
+            .AddOpenIddict()
+            .AddServer(options =>
+            {
+                options.AddDevelopmentEncryptionCertificate()
+                                   .AddDevelopmentSigningCertificate();
+
+                options.SetTokenEndpointUris("/connect/token");
+                options.UseAspNetCore().EnableTokenEndpointPassthrough();
+
+
+                options.SetLogoutEndpointUris("/connect/logout");
+                options.UseAspNetCore().EnableLogoutEndpointPassthrough();
+
+                options
+                    .AllowAuthorizationCodeFlow()
+                    .RequireProofKeyForCodeExchange()
+                    .SetAuthorizationEndpointUris("/connect/authorize");
+
+                options.UseAspNetCore().EnableAuthorizationEndpointPassthrough();
+
+
+                options.AllowPasswordFlow();
+
+                options.AllowRefreshTokenFlow();
+            });
+
+    }
+}
